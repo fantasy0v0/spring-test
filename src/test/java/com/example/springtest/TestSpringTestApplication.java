9c4bd2d40f41dade7310e63012a88a5290962eb6
fan <xiaofanms@outlook.com>
2024-12-11 17:04:03 +0800

    init

---
+++ src/test/java/com/example/springtest/TestSpringTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.springtest;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSpringTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(SpringTestApplication::main).with(TestcontainersConfiguration.class).run(args);
+  }
+
+}
